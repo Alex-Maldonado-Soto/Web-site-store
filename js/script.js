b282f8eb288e00c8c2691a8267bfaa934100fd08
Alexander Madonado <alexandermaldonadosoto@gmail.com>
2022-06-15 14:31:24 -0600

    primer commit desde el otro ubuntu

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const name = document.getElementById("name")
+const password = document.getElementById("password")
+const email = document.getElementById("email")
+const form = document.getElementsByName("form")
+const parrafo = document.getElementsById("warnings")
+
+
+form.addEventListener
