cd165e107a4ab3d053fdf5100894af9bb3ed672b
Alexander Madonado <alexandermaldonadosoto@gmail.com>
2022-06-15 21:59:05 -0600

    Termina la validacion de formularios solo falta cambiarle el password por texto general

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
-const name = document.getElementById("name")
-const password = document.getElementById("password")
+const nombre = document.getElementById("name")
+const pass = document.getElementById("password")
 const email = document.getElementById("email")
-const form = document.getElementsByName("form")
-const parrafo = document.getElementsById("warnings")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
 
 
-form.addEventListener
+form.addEventListener("submit", e=> {
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length <6){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+
+    console.log(regexEmail.test(email.value))
+
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+
+    if(pass.value.length < 8){
+        warnings += `La contrasena no es valida <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    } else {
+        parrafo.innerHTML = `Enviado`
+    }
+
+})
