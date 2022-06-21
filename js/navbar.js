f18c3350b54f014c0ddef842c8805afdd27cb506
Alexander Madonado <alexandermaldonadosoto@gmail.com>
2022-06-20 18:44:49 -0600

    Termine todo el responsive del sitio web solo falta corregir textos y posiblemente cambiarle la paleta de colores

---
+++ js/navbar.js
@@ -0,0 +1,17 @@
+// sliders[value-1].classList.toggle(`slider__body--show`);
+
+const navBarResponsive = document.querySelector(`nav`);
+
+function navBarToggle() {
+    navBarResponsive.classList.toggle(`nav-responsive`);
+}
+
+
+
+
+
+// navBarClick.addEventListener(`click`, ()=>changePosition(1));
+
+
+
+// const arrowAfter = document.querySelector(`#after`)
