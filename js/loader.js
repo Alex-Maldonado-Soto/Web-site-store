6e710db279c83a60be90587dfba584c7012abe93
Alexander Madonado <alexandermaldonadosoto@gmail.com>
2022-06-21 21:47:19 -0600

    coloque una pantalla de carga

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var container = document.getElementById(
+        `container_loader`);
+
+    container.style.visibility = `hidden`;
+    container.style.opacity = `0`;    
+
+}
